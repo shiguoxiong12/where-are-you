dd3e9666c5545b14b7a820821d6d6d1f201c35a6
shiguoxiong12 <13307091147@163.com>
2018-11-27 21:03:45 +0800

    去哪儿首次提交

---
+++ goWhere/go/src/view/home/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="header">
+        <div class="headerLeft">
+            <div class="iconfont">&#xe624;</div>
+        </div>
+        <div class="input">
+            <span class="iconfont">&#xe632;</span> 输入城市景点游玩主题</div>
+        <div class="headerRight ">
+             城市
+             <span class="iconfont right-icon">&#xe64a;</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/veribles.styl'
+.header
+  font-size 14px
+  width 100%
+  color #fff
+  text-align center
+  line-height .86rem
+  background-color $bgColor
+  display flex
+  .headerLeft
+      width:.86rem;
+      float:left;
+   .input
+      flex 1
+      background-color #fff
+      border-radius 5px
+      height .64rem
+      padding-left .2rem
+      margin-left .2rem
+      text-align left
+      margin-top .12rem
+      color #ccc
+      line-height .64rem
+ .headerRight
+      width:1.24rem;
+      float:right
+      .right-icon
+        font-size .24rem
+</style>
---
+++ goWhere/go/src/view/home/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+       <homeHead/>
+    </div>
+</template>
+
+<script>
+import homeHead from './components/Header.vue'
+    export default {
+        data(){
+            return{
+
+            }
+        },
+        components:{
+            homeHead
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
