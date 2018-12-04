8742b75e7a93a0b5d42515cdd435c44c6379d1db
shiguoxiong12 <13307091147@163.com>
2018-12-04 14:45:36 +0800

    城市列表完成

--- goWhere/go/src/view/city/components/city-list.vue
+++ goWhere/go/src/view/city/components/city-list.vue
@@ -61,14 +61,12 @@
              </div>
             
          </div>
-          <div class="area" v-for="(parent,index) in city" :key="index" :ref="parent.name">
+          <div class="area list" v-for="(parent,index) in city" :key="index" :ref="parent.name">
              <div class="title border-topbottom">{{parent.name}}</div>
              <div class="cityList" v-for="(item,index) in parent.val" :key="index">
                  <div class="item">{{item.name}}</div>
-               
              </div>
           </div>
-       
           </div>
             <div class="area cityArr" ref="cityArr">
               <ul ref="ul">
@@ -97,21 +95,34 @@
                 city:[],
                 current:0 ,
                 scroll:{},
-                startMove:false
+                startMove:false,
+                scrolY:0,
+                
             }
         },
         created(){
            axios.get('/api/city.json').then((data)=>{
                this.city=formater(data.data.city)
            })
-           setTimeout(()=>{
-            
-           },1000)
         },
         mounted(){
             setTimeout(()=>{
                this.init()
             },1000)  
+        },
+        watch:{
+          scrolY(newV,oldV){
+              this.scrolY=newV
+              for(let i=0;i<this.divHeight.length;i++){
+                  let curent=this.divHeight[i]+40
+                  let nextC=this.divHeight[i+1]-40
+                  if(curent<Math.abs(newV)&&Math.abs(newV)<nextC){
+                      console.log(i)
+                      this.current=i
+                  }
+              }
+              
+          }
         },
         methods:{
            handler1(e,index){
@@ -119,6 +130,20 @@
                   const curr=e.target.innerText;
                   const el=this.$refs[curr][0]
                   this.scroll.scrollToElement(el)     
+           },
+           calculate(arr,index){
+               console.log(arr)
+              
+                let val=0
+               if(index==0){
+                  val=arr[0]
+               }else{
+                   for(var a=0;a<=index;a++){
+                    val+=arr[a]
+                  }
+               }
+              
+               return val;
            },
            start(){
                   this.startMove=true
@@ -132,40 +157,44 @@
                    const m=this.cityArr[index]
                    const el=this.$refs[m][0]
                    this.scroll.scrollToElement(el)
-
                 } 
            },
            end(){
                this.start=false
            },
            init(){
-               let wrapper = this.$refs.wraper
+              console.log(this.divHeight)
+                let wrapper = this.$refs.wraper
                 this.scroll = new BetterScroll(wrapper,{
                    click:true,
                    tap:true,
                    probeType:2
                })
                this.scroll.on('scroll',(e)=>{
-                   let y=e.y; 
-                   console.log(y)
+                   this.scrolY=e.y; 
+                  
                })
            }
         },
         computed:{
             cityArr(){
                let arr=[]
-              this.city.map((i)=>{
+               this.city.map((i)=>{
                  arr.push(i.name)
                })
-               console.log(arr)
                return arr;
             },
             divHeight(){
                 let height=[];
-                let content=document.querySelectorAll("#content .area")
+                let content=document.querySelectorAll("#content .list")
                  content.forEach((i)=>{
                     height.push(i.offsetTop)
                  })
+                //  let wraper=[]
+                // for(var i=0;i<height.length;i++){
+                    
+                //     wraper.push(this.calculate(height,i))
+                // }
                  return height;
             }
         }
